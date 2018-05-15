975613ff49ae96c49922ee53cb29a37a87f96eb2
Keigh Rim <keigh.rim@gmail.com>
2018-05-15 16:21:00 -0400

    updated stanford tool, including relation extraction

--- stanford/tool_conf.groovy
+++ stanford/tool_conf.groovy
@@ -1,20 +1,21 @@
-def tokenizer = 'stanford/tokenizer_2.1.0.xml'
-def splitter  = 'stanford/splitter_2.1.0.xml'
-def tagger = 'stanford/tagger_2.1.0.xml'
-def names = 'stanford/ner_2.1.0.xml'
-def parser = 'stanford/parser_2.1.0.xml'
-def depparser = 'stanford/depparser_2.1.0.xml'
-def coref = 'stanford/coref_2.1.0.xml'
+def tokenizer = 'stanford/tokenizer_3.0.0.xml'
+def splitter  = 'stanford/splitter_3.0.0.xml'
+def tagger = 'stanford/tagger_3.0.0.xml'
+def names = 'stanford/ner_3.0.0.xml'
+def parser = 'stanford/parser_3.0.0.xml'
+def depparser = 'stanford/depparser_3.0.0.xml'
+def coref = 'stanford/coref_3.0.0.xml'
+def relextr = 'stanford/relextr_3.0.0.xml'
 
 stanford 'Stanford NLP', {
-	tools tokenizer, splitter, tagger, names, parser, depparser, coref
+	tools tokenizer, splitter, tagger, names, parser, depparser, coref, relextr
 }
 
 tokenizers { tool tokenizer }
 splitters { tool splitter }
 taggers { tool tagger }
 ner { tool names }
-parsers { tool parser }
-// depparser { tool depparser }
+parsers { tools parser, depparser }
+rel_extr { tool relextr }
 // coref { tool coref }
  
